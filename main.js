45fadc040ff4f7d23995ebe4e57aeedb7073985c
Matt Chang <matthewchang3150@hotmail.com>
2016-09-01 23:18:31 -0700

    Attempt to create objects using canvas

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
 $(document).ready(function(){
 
-  // Fadeout instructions after 7 seconds
-  setTimeout(function(){
-    $('#instructions').fadeOut(2000);
-  },5000)
-
   // Defining variables globally
   var degree = 0;
   var speed = 0;
@@ -14,22 +9,43 @@ $(document).ready(function(){
   var speedFractionX = parseFloat($('#rocket').css('transform').split(",")[1])
   var speedFractionY = parseFloat($('#rocket').css('transform').split(",")[3])
 
+  $('#game-canvas').attr('width',$(window).width()*0.98);
+  $('#game-canvas').attr('height',$(window).height()*0.98);
+
+  var img = new Image();
+  // img.onload = function(){
+  //   ctx.drawImage(document.getElementById('rocket'));
+  // };
+  img.src = "rocket.png";
+
+
+  // Fadeout instructions after 7 seconds
+  setTimeout(function(){
+    $('#instructions').fadeOut(2000);
+  },5000)
+
+
+
 
   // Defining keyboard inputs
   $(document).on('keydown', function(event) {
     var key = String.fromCharCode(event.which);
     shipLeft = parseInt($('#rocket').css('left'));
     shipTop = parseInt($('#rocket').css('top'));
+    console.log(event.which);
     if (key === "W") {
       speed += 10;
     } else if(key === "S") {
       speed -= 20;
     } else if(key === "A") {
-      degree -= 10;
+      degree -= 15;
       $('#rocket').css("transform", 'rotateZ(' + degree + 'deg)');
     } else if(key === "D") {
-      degree += 10;
+      degree += 15;
       $('#rocket').css("transform", 'rotateZ(' + degree + 'deg)');
+    }
+    if (event.which == 32) {
+      var fire = setInterval(update, 20);
     }
   });
 
@@ -65,6 +81,51 @@ $(document).ready(function(){
       $('#speed').html(speed);
     }
 
+
+
   },50)
+
+  var canvas = document.getElementById("game-canvas"),
+      ctx = canvas.getContext("2d");
+
+  var W = 1300,
+      H = 700;
+
+  canvas.height = H; canvas.width = W;
+
+
+  var ball = {};
+  var cooldown = setInterval(function(){
+    ball = {
+      x: parseInt($('#rocket').css("left")) + 20,
+      y: parseInt($('#rocket').css("top")) + 10,
+      radius: 5,
+      color: "white",
+      vx: speedFractionX,
+      vy: speedFractionY,
+      draw: function() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.closePath();
+      }
+    };
+  },1000)
+
+
+  function clearCanvas() {
+    ctx.clearRect(0, 0, W, H);
+  }
+
+  function update() {
+    clearCanvas();
+    ball.draw();
+    ball.y -= 30*speedFractionY;
+    ball.x += 30*speedFractionX;
+  }
+
+
+
 
 });
